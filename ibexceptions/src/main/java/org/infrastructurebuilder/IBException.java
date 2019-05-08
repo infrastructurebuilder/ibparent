9baeed676017da3040db4c1798efa9c2cc8d27b9
mykelalvis <mykel.alvis@gmail.com>
2019-05-08 13:30:02 -0500

    Initial commit

---
+++ ibexceptions/src/main/java/org/infrastructurebuilder/IBException.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright © 2019 admin (admin@infrastructurebuilder.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.infrastructurebuilder;
+
+import com.mscharhag.et.ET;
+import com.mscharhag.et.ExceptionTranslator;
+
+public class IBException extends RuntimeException {
+
+  /**
+   *
+   */
+  private static final long serialVersionUID = -3615731463803876276L;
+  public static ExceptionTranslator cet = ET.newConfiguration().translate(Exception.class).to(IBException.class).done();
+
+  public IBException() {
+  }
+
+  public IBException(final String message) {
+    super(message);
+  }
+
+  public IBException(final String message, final Throwable cause) {
+    super(message, cause);
+  }
+
+  public IBException(final String message, final Throwable cause, final boolean enableSuppression,
+      final boolean writableStackTrace) {
+    super(message, cause, enableSuppression, writableStackTrace);
+  }
+
+  public IBException(final Throwable cause) {
+    super(cause);
+  }
+
+}
